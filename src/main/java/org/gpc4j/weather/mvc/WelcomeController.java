279de45609f074fe81d1e0c56a166baaa0f88e16
Lyle Harris <receipts@gpc4j.org>
2021-02-07 13:39:45 -0800

    No path to GetMapping

--- src/main/java/org/gpc4j/weather/mvc/WelcomeController.java
+++ src/main/java/org/gpc4j/weather/mvc/WelcomeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WelcomeController {
 
-  @GetMapping("/")
+  @GetMapping()
   public String homePage(Model model) {
     return "index.xhtml";
   }
