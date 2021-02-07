86498270510de6c8f1f557663fc84ccf2a259a46
Lyle Harris <receipts@gpc4j.org>
2021-02-07 09:12:55 -0800

    Inititial commit

---
+++ src/test/java/org/gpc4j/weather/StationBeanTest.java
@@ -0,0 +1,19 @@
+
+package org.gpc4j.weather;
+
+
+import org.junit.jupiter.api.Test;
+
+public class StationBeanTest {
+
+
+  /**
+   * Need at least one test to satisfy Jenkins Pipeline.
+   */
+  @Test
+  public void atLeastOne() {
+
+  }
+
+
+}
