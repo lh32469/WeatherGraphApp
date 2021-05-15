27475d18b2214ed97ab86f137b25e05a3646692f
Lyle Harris <lyle.harris@gcpay.com>
2021-05-15 16:18:54 -0700

    Added /ping endpoint

--- src/main/java/org/gpc4j/weather/ApplicationMain.java
+++ src/main/java/org/gpc4j/weather/ApplicationMain.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.InetAddress;
@@ -28,4 +29,11 @@ public class ApplicationMain {
 
     SpringApplication.run(ApplicationMain.class, args);
   }
+
+  @GetMapping(path = "ping")
+  public String ping() {
+    return "pong\n";
+  }
+
+
 }
